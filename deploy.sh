693a21777a31cecb2aa54ebe3ab31b8a7b705767
Даниил Пронин <mail@grawl.ru>
2016-09-04 22:21:34 +1000

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,16 @@
 set -e # Exit with nonzero exit code if anything fails
 git checkout master
 npm run gulp
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
+if [[ `git status --porcelain` ]]; then
+	echo "There are changes to publish!"
+	git status
+	exit 0
+	git add .
+	git remote add deployment https://${USER}:${PASSWORD}@github.com/${TRAVIS_REPO_SLUG}.git
+	git config user.name "Travis CI"
+	git commit -m "Travis CI $TRAVIS_COMMIT"
+	git push -u deployment master
+else
+	echo "No changes to the output on this push; exiting."
     exit 0
 fi
-git add .
-git remote add deployment https://$USER:$PASSWORD@github.com/$TRAVIS_REPO_SLUG.git
-git config user.name "Travis CI"
-git commit -m "Travis CI $TRAVIS_COMMIT"
-git push -u deployment master
